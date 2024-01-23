5049c0301bbec93161f148f88edd573b41ed5dfa
Evan Tobin <evantobin91384@gmail.com>
2024-01-22 17:14:33 -0800

    Added S3uploader component dependency

--- components/file_transfer_to_s3/artifacts/file_transfer_to_s3.py
+++ components/file_transfer_to_s3/artifacts/file_transfer_to_s3.py
@@ -3,7 +3,7 @@ import os
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-import boto3
+from greengrasssdk import IPCClient
 
 
 class MyHandler(FileSystemEventHandler):
@@ -14,23 +14,27 @@ class MyHandler(FileSystemEventHandler):
         if event.event_type == 'created' and os.path.isfile(file_path):
             print(f"Detected new file: {file_path}")
 
-            s3 = boto3.client('s3')
             file_name = os.path.basename(file_path)
             folder_name = "inbox"
             key = f'{folder_name}/{file_name}'
 
             # Upload the file to S3
             try:
-                with open(file_path, 'rb') as data:
-                    s3.upload_fileobj(data, bucket_name, key, ExtraArgs={
-                                      'ContentType': 'application/pdf'})
+                s3_upload_request = {
+                    "localFilePath": file_path,
+                    "bucket": bucket_name,
+                    "key": key
+                }
+                response = ipc_client.send_request(
+                    "aws.greengrass.S3Uploader", s3_upload_request)
                 print(f"Uploaded {file_name} to {bucket_name}/{key}")
+                print(f"Upload response: {response}")
 
                 # Delete the local file
                 os.remove(file_path)
                 print(f"Deleted local file: {file_path}")
             except Exception as e:
-                print(f"Error occurred: {e}")
+                print(f"IPC request error: {e}")
 
     def on_created(self, event):
         self.process(event)
@@ -41,6 +45,8 @@ if __name__ == "__main__":
     bucket_name = sys.argv[2]
     print("Path: %s!" % sys.argv[1])
     print("Bucket Name: %s!" % sys.argv[2])
+
+    ipc_client = IPCClient()
 
     observer = Observer()
     observer.schedule(MyHandler(), path, recursive=False)
